7d45ecb865f51ca3564734982d2062cb00b6ef7c
Patrick Kim <pkim050@gmail.com>
2019-05-24 14:20:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(string) {
+  bestCustomer = string;
+}
+
+const leastFavoriteCustomer = "pat";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "sam";
+}
