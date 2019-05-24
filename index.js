bbfa4b5ac5078fa77d5db888a7f17c2b9e5a84b2
Patrick Kim <pkim050@gmail.com>
2019-05-24 14:20:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 var customerName = "bob";
 
 function upperCaseCustomerName() {
-  customerName.toUpperCase();
+  customerName = customerName.toUpperCase();
+  return customerName
 }
 
 function setBestCustomer() {
