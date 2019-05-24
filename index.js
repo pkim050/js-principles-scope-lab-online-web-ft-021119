fafa1621a6d8280bf3a9b7ca47039f01f62b8adf
Patrick Kim <pkim050@gmail.com>
2019-05-24 14:18:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName;
+
+function upperCaseCustomerName() {
+  customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(string) {
+  bestCustomer = string;
+}
+
+const leastFavoriteCustomer = "pat";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "sam";
+}
